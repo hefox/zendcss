f019dfb2554329288fc0f4792427f75621370ed4
Fox <fox@Alexander-Grape-labtop.local>
2010-07-18 01:36:36 -0400

    Colours

---
+++ css/page-backgrounds.css.php
@@ -0,0 +1,49 @@
+/* $Id$ */
+
+/**
+ * @file
+ * Page Background Styling
+ *
+ * The default layout method of Zen doesn't give themers equal-height columns.
+ * However, equal-height columns are difficult to achieve and totally
+ * unnecessary. Instead, use the Faux Columns method described in the following
+ * ALA article:
+ *   http://www.alistapart.com/articles/fauxcolumns/
+ */
+
+<?php
+  $selectors = array(
+    'body' => 'body',
+    'footer' => '#footer',
+    'page' => '#page',
+    'header' => '#header',
+    'header_section' => '#header .section',
+    'footer_section' => '#footer .section',
+    'main' => '#main',
+    'page_wrapper' => '#page-wrapper',
+  );
+  $colors = dcss('colors');
+  if ($colors) {
+    foreach($colors as $key => $values) {
+      $css = array(
+        'selector' => $selectors[$key],
+        'styles' => array(),
+      );
+      foreach($values as $type => $color) { 
+        if ($color != '' && $color != 'none') {
+          switch($type) {
+            case 'font':
+              $css['styles']['color'] = $color;
+              break;
+            case 'background':
+              $css['styles']['background-color'] = $color;
+            break;
+          }
+        }
+      }
+      if ($css['styles']) {
+        echo zendcss_format($css);
+      }
+    }
+  }
+?>
